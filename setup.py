07a139a0ce49fb088c7de9498a0db589e86ad810
kylecorry31 <kylecorry31@gmail.com>
2015-09-12 09:31:37 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,9 @@ setup(
     name='raspy',
     version='0.5',
     description='Python library to interact with sensors and actuators using a Raspberry Pi.',
-    url='http://github.com/kylecorry31/Raspy',
+    url='http://github.com/kylecorry31/Raspy2',
     author='Kyle Corry',
     author_email='kylecorry31@gmail.com',
-    packages=['raspy'],
     install_requires=[
         "RPi.GPIO >= 0.5.0"
     ]
